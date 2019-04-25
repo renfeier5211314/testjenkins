6e39a157d147294496bd357c7160d85e535e44eb
ytly <renfeier5211314@126.com>
2019-04-25 15:09:03 +0800

    测试

---
+++ model1/src/main/java/com/example/model1/Model1Application.java
@@ -0,0 +1,14 @@
+package com.example.model1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Model1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Model1Application.class, args);
+//        System.out.println(TestUtil.test());
+    }
+
+}
---
+++ model2/src/main/java/com/example/model2/Model2Application.java
@@ -0,0 +1,14 @@
+package com.example.model2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Model2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Model2Application.class, args);
+//        System.out.println(TestUtil.test());
+    }
+
+}
