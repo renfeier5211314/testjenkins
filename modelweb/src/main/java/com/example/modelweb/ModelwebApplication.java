ae2075d66a7170f26ac3d107b8acdca374622e77
ytly <renfeier5211314@126.com>
2019-04-25 17:55:52 +0800

    测试

---
+++ modelweb/src/main/java/com/example/modelweb/ModelwebApplication.java
@@ -0,0 +1,15 @@
+package com.example.modelweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModelwebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModelwebApplication.class, args);
+
+        System.out.println("我是model    333333");
+    }
+
+}
